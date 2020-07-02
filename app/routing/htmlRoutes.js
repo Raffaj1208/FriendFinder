e7ee12edb7438af9cec19db2b12ae517d90531db
Raffaj1208 <61484778+Raffaj1208@users.noreply.github.com>
2020-07-01 17:14:31 -0700

    ..

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,14 @@
+//...
+let express = require('express');
+let app = express();
+let path = require('path');
+//...
+module.exports = function() {
+app.get('/survey', function(request, response) {
+    response.sendfile(path.join(__dirname, '../public/survey.html'));
+});
+//...
+app.get('*', function(request,response){
+    response.sendFile(path.join(__dirname, '../public/home.html'));
+});
+};
