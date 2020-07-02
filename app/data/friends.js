dd2147ac5cb8e05765bea426eb8d421ef37af0f5
Raffaj1208 <61484778+Raffaj1208@users.noreply.github.com>
2020-07-01 17:27:08 -0700

    creating friends array

---
+++ app/data/friends.js
@@ -0,0 +1,34 @@
+let friends = [
+    {
+        name:'Jim',
+        photo: '',
+        scores: ['','','','','','','','','','']
+    },
+    {
+        name: 'Dwight',
+        photo: '',
+        scores: ['','','','','','','','','','']
+    },
+    {
+        name: 'Andy',
+        photo: '',
+        scores: ['','','','','','','','','','']
+    },
+    {
+        name: 'Pam',
+        photo: '',
+        scores: ['','','','','','','','','','']
+    },
+    {
+        name: 'Stanley',
+        photo: '',
+        scores: ['','','','','','','','','','']
+    },
+    {
+        name: 'Erin',
+        photo: '',
+        scores: ['','','','','','','','','','']
+    },
+];
+
+module.exports = friends;
