f14ef4b2588dd954c4ba1190ee524b9d28ab42ee
Raffaj1208 <61484778+Raffaj1208@users.noreply.github.com>
2020-07-10 16:40:04 -0700

    Images still not working, app now opens survey and returns match

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,32 +1,63 @@
-let friends = require('../data/friends');
-
-module.exports = function (app) {
-    app.get('/api/friends', function(request, response) {
-        response.json(friends);
+//..
+var friendsData = require("../data/friends");
+//..
+calcTotalDifference = function(user, candidate) {
+    var totalDiff = 0;
+    var userScores = user.scores.map(function (x) {
+        return parseInt(x, 10);
     });
-    app.post('/api/friends', function(request, response){
-        let yourFriend = {
-            name: '',
-            photo: '',
-            differance: 1000
-        };
-        let newUser = request.body;
-        for (let i = 0; i < friends.length; i++){
-            let currentFriend = friends[i];
-            let scoreDifference = 0;
-        
-        for (let j = 0; j < currentFriend.scores.length; j++){
-            let friendsScore = currentFriend.scores[j];
-            let userScore = newUser.scores[j];
-            scoreDifference += Math.abs(userScore - friendsScore);
-        }
-        if (scoreDifference <= yourFriend.differance){
-            yourFriend.name = friendsScore.name;
-            yourFriend.photo = friendsScore.photo;
-            yourFriend.differance = scoreDifference;
-        }
+    console.log("userScores: ", userScores.join(" "));
+  //..
+    var candidateScores = candidate.scores.map(function (x) {
+        return parseInt(x, 10);
+    });
+    console.log("candidateScores: ", candidateScores.join(" "));
+//..
+    for (var i = 0; i < userScores.length; i++) {
+        totalDiff += Math.abs(userScores[i] - candidateScores[i]);
     }
-    response.json(yourFriend);
-    friends.push(newUser);
+    console.log("totalDiff: ", totalDiff);
+//..
+    return {
+        name: candidate.name,
+        photo: candidate.photo,
+        totalDiff: totalDiff
+    };
+}
+
+//..
+module.exports = function (app) {
+
+    app.get("/api/friends", function (req, res) {
+        res.json(friendsData);
+    });
+
+    app.post("/api/friends", function (req, res) {
+        
+        var currentUser = req.body;
+        console.log("currentUser: ", currentUser);
+        var candidateArray = [];
+            for (var i = 0; i < friendsData.length; i++) {
+            candidateArray.push(calcTotalDifference(currentUser, friendsData[i]));
+        }
+
+        candidateArray.sort(function (a, b) {
+        var totalDiff1 = a.totalDiff;
+        var totalDiff2 = b.totalDiff;
+            if (totalDiff1 < totalDiff2) {
+                return -1;
+            } else if (totalDiff1 > totalDiff2) {
+                return 1;
+            }
+            return 0;
+        });
+
+        for (var i = 0; i < candidateArray.length; i++) {
+           console.log(candidateArray[i].name, candidateArray[i].photo, candidateArray[i].totalDiff);
+        }
+        friendsData.push(currentUser);
+        res.json(candidateArray[0]);
+        console.log("Most compatible friend: ", candidateArray[0].name, candidateArray[0].photo, candidateArray[0].totalDiff);
+
     });
 };
