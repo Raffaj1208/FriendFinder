5ed886d5f39a88e5790bda4d34d669eb5cc9b11d
Raffaj1208 <61484778+Raffaj1208@users.noreply.github.com>
2020-07-10 11:59:43 -0700

    ..

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ let path = require('path');
 let bodyParser = require('body-parser');
 let PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.set('PORT', process.env.PORT || 3000);
 
 require('./app/routing/htmlRoutes')(app);
 require('./app/routing/apiRoutes')(app);
