aebd8796a8605f511dd751cdcc3b42420d68c93b
Raffaj1208 <61484778+Raffaj1208@users.noreply.github.com>
2020-07-10 17:19:48 -0700

    images wont display

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,32 +1,32 @@
 let friends = [
     {
         name:'Jim',
-        photo: '../Images/jim-Copy.jpg',
+        photo: '../asset/Images/jim-Copy.jpg',
         scores: ['5','1','5','1','5','1','3','1','5','3']
     },
     {
         name: 'Dwight',
-        photo: '../Images/dwight-Copy.jpg',
+        photo: '../assets/Images/dwight-Copy.jpg',
         scores: ['5','5','1','1','1','5','5','3','1','1']
     },
     {
         name: 'Andy',
-        photo: '../Images/andy,jpg',
+        photo: '../assets/Images/andy,jpg',
         scores: ['5','1','3','1','1','1','3','1','5','5']
     },
     {
         name: 'Pam',
-        photo: '../Images/Pam_Beesley-Copy,jpg',
+        photo: '../assets/Images/Pam_Beesley-Copy,jpg',
         scores: ['3','2','5','1','4','1','3','1','5','5']
     },
     {
         name: 'Stanley',
-        photo: '../Images/Stanley_Hudson - Copy.jpg',
+        photo: '../assets/Images/Stanley_Hudson - Copy.jpg',
         scores: ['1','2','5','3','1','3','3','5','3','1']
     },
     {
         name: 'Erin',
-        photo: '../Images/erin-Copy.jpg',
+        photo: '../assets/Images/erin-Copy.jpg',
         scores: ['3','3','3','1','4','1','2','3','5','5']
     },
 ];
